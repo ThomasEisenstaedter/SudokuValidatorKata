c583aec3f01755b7b2adae5c68dfc59f24f728ca
Eisenstaedter, Thomas (SE-A/34) <thomas.eisenstaedter@volkswagen.de>
2024-04-25 14:50:03 +0200

    Test 14_invalid boxes

--- src/main/kotlin/SudokuValidator.kt
+++ src/main/kotlin/SudokuValidator.kt
@@ -1,17 +1,15 @@
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
-
 fun validate(board: Array<Array<Int>>): Boolean {
-      return when {
-            board.all { it.distinct().size != 9 } -> false
-            board.all { it.contentEquals(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)) } -> false
-            notOnlyValidNumbers(board) -> false
-            duplicateValuesInColumns(board) -> false
-            else -> true
-      }
+    return when {
+        board.all { it.distinct().size != 9 } -> false
+        board.all { it.contentEquals(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)) } -> false
+        notOnlyValidNumbers(board) -> false
+        duplicateValuesInColumns(board) -> false
+        invalidBoxes(board) -> false
+        else -> true
+    }
 }
 
-private fun notOnlyValidNumbers(board: Array<Array<Int>>) : Boolean {
+private fun notOnlyValidNumbers(board: Array<Array<Int>>): Boolean {
     if (board.all { row ->
             row
                 .all { number -> ("[1-9]".toRegex().matches(number.toString())) }
@@ -22,23 +20,43 @@ private fun notOnlyValidNumbers(board: Array<Array<Int>>) : Boolean {
 }
 
 private fun duplicateValuesInColumns(board: Array<Array<Int>>): Boolean {
-            for (number in board.indices) {
-                println(number)
-                  if(listOf(
-                          board[0][number],
-                          board[1][number],
-                          board[2][number],
-                          board[3][number],
-                          board[4][number],
-                          board[5][number],
-                          board[6][number],
-                          board[7][number],
-                          board[8][number],
-                 ).toSet().size !=9) {
-                        return true
-                 }
-            }
-      return false
+    for (number in board.indices) {
+        if (listOf(
+                board[0][number],
+                board[1][number],
+                board[2][number],
+                board[3][number],
+                board[4][number],
+                board[5][number],
+                board[6][number],
+                board[7][number],
+                board[8][number],
+            ).toSet().size != 9
+        ) {
+            return true
+        }
+    }
+    return false
+}
+
+private fun invalidBoxes(board: Array<Array<Int>>): Boolean {
+    for (number in board.indices step 3) {
+        if (listOf(
+                board[0 + number][number],
+                board[1 + number][number],
+                board[2 + number][number],
+                board[0 + number][1+number],
+                board[1 + number][1+number],
+                board[2 + number][1+number],
+                board[0 + number][2+number],
+                board[1 + number][2+number],
+                board[2 + number][2+number],
+            ).toSet().size != 9
+        ) {
+            return true
+        }
+    }
+    return false
 }
 
 
--- src/test/kotlin/SudokuValidatorTest.kt
+++ src/test/kotlin/SudokuValidatorTest.kt
@@ -215,21 +215,21 @@ class MissingLetterTests {
         assertEquals(false, validate(board))
     }
 
-//    @Test
-//    fun `invalid boxes`() {
-//        val board = arrayOf(
-//            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
-//            arrayOf(2, 3, 4, 5, 6, 7, 8, 9, 1),
-//            arrayOf(3, 4, 5, 6, 7, 8, 9, 1, 2),
-//            arrayOf(4, 5, 6, 7, 8, 9, 1, 2, 3),
-//            arrayOf(5, 6, 7, 8, 9, 1, 2, 3, 4),
-//            arrayOf(6, 7, 8, 9, 1, 2, 3, 4, 5),
-//            arrayOf(7, 8, 9, 1, 2, 3, 4, 5, 6),
-//            arrayOf(8, 9, 1, 2, 3, 4, 5, 6, 7),
-//            arrayOf(9, 1, 2, 3, 4, 5, 6, 7, 8)
-//        )
-//        assertEquals(false, validate(board))
-//    }
+    @Test
+    fun `invalid boxes`() {
+        val board = arrayOf(
+            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            arrayOf(2, 3, 4, 5, 6, 7, 8, 9, 1),
+            arrayOf(3, 4, 5, 6, 7, 8, 9, 1, 2),
+            arrayOf(4, 5, 6, 7, 8, 9, 1, 2, 3),
+            arrayOf(5, 6, 7, 8, 9, 1, 2, 3, 4),
+            arrayOf(6, 7, 8, 9, 1, 2, 3, 4, 5),
+            arrayOf(7, 8, 9, 1, 2, 3, 4, 5, 6),
+            arrayOf(8, 9, 1, 2, 3, 4, 5, 6, 7),
+            arrayOf(9, 1, 2, 3, 4, 5, 6, 7, 8)
+        )
+        assertEquals(false, validate(board))
+    }
 //
 //    @Test
 //    fun `invalid sudoku 1`() {

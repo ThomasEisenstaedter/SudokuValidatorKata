efe4baebcba102f0a8ae6930b03ffe59d3afa40e
Eisenstaedter, Thomas (SE-A/34) <thomas.eisenstaedter@volkswagen.de>
2024-04-25 13:35:55 +0200

    Test 12_invalid because of 0 instead of 9

--- src/main/kotlin/SudokuValidator.kt
+++ src/main/kotlin/SudokuValidator.kt
@@ -1,12 +1,20 @@
 import com.sun.org.apache.xpath.internal.operations.Bool
+import java.util.*
 
 fun validate(board: Array<Array<Int>>): Boolean {
       return when {
             board.all { it.distinct().size != 9 } -> false
             board.all { it.contentEquals(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)) } -> false
+            !onlyValidNumbers(board) -> false
             else -> true
       }
 }
+
+private fun onlyValidNumbers(board: Array<Array<Int>>) = board
+      .all { row -> row.all { e -> ("[1-9]".toRegex().matches(e.toString()))}}
+
+
+
 
 
 
--- src/test/kotlin/SudokuValidatorTest.kt
+++ src/test/kotlin/SudokuValidatorTest.kt
@@ -183,21 +183,21 @@ class MissingLetterTests {
         assertEquals(true, validate(board))
     }
 
-//    @Test
-//    fun `invalid because of 0 instead of 9`() {
-//        val board = arrayOf(
-//            arrayOf(8, 4, 7, 2, 6, 5, 1, 0, 3),
-//            arrayOf(1, 3, 6, 7, 0, 8, 2, 4, 5),
-//            arrayOf(0, 5, 2, 1, 4, 3, 8, 6, 7),
-//            arrayOf(4, 2, 0, 6, 7, 1, 5, 3, 8),
-//            arrayOf(6, 7, 8, 5, 3, 2, 0, 1, 4),
-//            arrayOf(3, 1, 5, 4, 8, 0, 7, 2, 6),
-//            arrayOf(5, 6, 4, 0, 1, 7, 3, 8, 2),
-//            arrayOf(7, 8, 1, 3, 2, 4, 6, 5, 0),
-//            arrayOf(2, 0, 3, 8, 5, 6, 4, 7, 1)
-//        )
-//        assertEquals(false, validate(board))
-//    }
+    @Test
+    fun `invalid because of 0 instead of 9`() {
+        val board = arrayOf(
+            arrayOf(8, 4, 7, 2, 6, 5, 1, 0, 3),
+            arrayOf(1, 3, 6, 7, 0, 8, 2, 4, 5),
+            arrayOf(0, 5, 2, 1, 4, 3, 8, 6, 7),
+            arrayOf(4, 2, 0, 6, 7, 1, 5, 3, 8),
+            arrayOf(6, 7, 8, 5, 3, 2, 0, 1, 4),
+            arrayOf(3, 1, 5, 4, 8, 0, 7, 2, 6),
+            arrayOf(5, 6, 4, 0, 1, 7, 3, 8, 2),
+            arrayOf(7, 8, 1, 3, 2, 4, 6, 5, 0),
+            arrayOf(2, 0, 3, 8, 5, 6, 4, 7, 1)
+        )
+        assertEquals(false, validate(board))
+    }
 //
 //    @Test
 //    fun `invalid because of duplicate 3 in eighth column`() {
